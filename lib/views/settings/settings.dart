fde30098ac50f7ef24508efa4e288fa0263dad80
OrganomagnesiumHalide <99297401+OrganomagnesiumHalide@users.noreply.github.com>
2022-12-06 18:57:11 -0800

    Added settings page

--- lib/views/settings/settings.dart
+++ lib/views/settings/settings.dart
@@ -1,5 +1,6 @@
 import 'package:control_panel/data_structures/main_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class Settings extends MainWidget {
   final bool darkTheme;
@@ -8,39 +9,41 @@ class Settings extends MainWidget {
       {super.key, required this.darkTheme, required this.smallDevice});
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("hubs"));
-  }
-
-  @override
-  Widget getSideBar() {
-    return ListView(
-      children: [
-        ListTile(
-          title: const Text('Fridge 1'),
-          onTap: () {},
-        ),
-        ListTile(
-          title: const Text('Fridge 2'),
-          onTap: () {},
-        ),
-        ListTile(
-          title: const Text('Fridge 3'),
-          onTap: () {},
-        ),
-        ListTile(
-          title: const Text('Fridge 4'),
-          onTap: () {},
-        ),
-        ListTile(
-          title: const Text('Fridge 5'),
-          onTap: () {},
+    return SettingsList(
+      sections: [
+        SettingsSection(
+          title: const Text('General'),
+          tiles: [
+            SettingsTile.navigation(
+              title: const Text('Register Phone'),
+              leading: const Icon(Icons.phone),
+              description: const Text('Register a telephone for alerts'),
+              onPressed: (context) {
+                debugPrint("Hihihih");
+              },
+            ),
+            SettingsTile.navigation(
+              title: const Text('Licensing information'),
+              //leading: const Icon(Icons.license),
+              description: const Text(
+                  'Read all the Open Source licenses that are applicable'),
+              onPressed: (context) {
+                debugPrint("Hihihih");
+              },
+            ),
+          ],
         ),
       ],
     );
   }
 
   @override
+  Widget getSideBar() {
+    return Container();
+  }
+
+  @override
   bool hasSideBar() {
-    return true;
+    return false;
   }
 }
