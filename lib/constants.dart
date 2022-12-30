7a2cffbce7102ae3645d1c1b998630c9dd323764
OrganomagnesiumHalide <99297401+OrganomagnesiumHalide@users.noreply.github.com>
2022-12-29 20:23:05 -0800

    Moving to AWS

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/foundation.dart';
 
-const remoteHttpDomain = kDebugMode
-    ? "http://192.168.1.164:1234"
-    : "https://fridgigator-001.fly.dev";
-const remoteWsDomain =
-    kDebugMode ? "ws://192.168.1.164:1234" : "wss://fridgigator-001.fly.dev";
+const remoteHttpDomain =
+    kDebugMode ? "http://192.168.1.164:1234" : "detoirhbf2f8n.cloudfront.net";
+const remoteWsDomain = kDebugMode
+    ? "ws://192.168.1.164:1234"
+    : "wss://detoirhbf2f8n.cloudfront.net";
 
 const clientID = kDebugMode ? "ceaa445aaef178c6cba4" : "30bf4172998cc4ec684e";
